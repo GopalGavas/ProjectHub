380427c8f6b3fe06eaa37c443511f29536bf34ee
Gopal Gavas <gopalgavas7@gmail.com>
2025-10-18 15:47:51 +0530

    add cookie-parser middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 app.use("/auth", authRouter);
+app.use("/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
